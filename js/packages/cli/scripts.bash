cd1c3bcae8aeb68b84641e6580a014032e9b7d01
Eduardo Campos de Souza <eduardofidera@gmail.com>
2022-02-10 05:49:30 -0300

    feat: update traits

--- js/packages/cli/scripts.bash
+++ js/packages/cli/scripts.bash
@@ -1,8 +1,8 @@
 # add numbered ordering
-new=0;for i in all/*.json; do mv "$i" all/"$new.json"; FILENAME=${i/.json/""}; mv "$FILENAME.png" all/"$new.png"; ((new++)); done
+new=0;for i in assets/*.json; do mv "$i" assets/"$new.json"; FILENAME=${i/.json/""}; mv "$FILENAME.png" assets/"$new.png"; ((new++)); done
 
 # edit JSON
-for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.image = "'$INDEX'.png" | .properties.files[0].uri = "'$INDEX'.png"' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
+for i in assets/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/assets\//""}; jq '.image = "'$INDEX'.png" | .properties.files[0].uri = "'$INDEX'.png"' assets/$INDEX.json > tmp.$$.json && mv tmp.$$.json assets/$INDEX.json; done
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.collection.name = "Elves of Vivendell"' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.symbol = "ELVE"' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.properties.creators[0].address = "2VFC2JSWqgvdrUft1VN2unRSA5PcRfb5QDWbNQazGJKg" | .properties.creators[0].share = 100' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done

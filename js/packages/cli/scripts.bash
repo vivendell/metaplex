57cd529dbc8c920e8940c7fff8c11eb0b5c75139
Eduardo Campos de Souza <eduardofidera@gmail.com>
2022-02-10 03:49:56 -0300

    feat: add shuffle script

--- js/packages/cli/scripts.bash
+++ js/packages/cli/scripts.bash
@@ -1,10 +1,12 @@
-for i in assets_mw/*.png; do uuid=$(uuidgen); mv "$i" assets_mw/"$uuid.png"; FILENAME=${i/.png/""};  mv "$FILENAME.json" assets_mw/"$uuid.json"; done
+# add numbered ordering
+new=0;for i in all/*.json; do mv "$i" all/"$new.json"; FILENAME=${i/.json/""}; mv "$FILENAME.png" all/"$new.png"; ((new++)); done
 
-new=0;for i in assets_fw/*.json; do mv "$i" all/"$new.json"; FILENAME=${i/.json/""}; mv "$FILENAME.png" all/"$new.png"; ((new++)); done
-
+# edit JSON
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.image = "'$INDEX'.png" | .properties.files[0].uri = "'$INDEX'.png"' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
-
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.collection.name = "Elves of Vivendell"' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
-
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.symbol = "ELVE"' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
 for i in all/*.png;  do FILENAME=${i/.png/""}; INDEX=${FILENAME/all\//""}; jq '.properties.creators[0].address = "2VFC2JSWqgvdrUft1VN2unRSA5PcRfb5QDWbNQazGJKg" | .properties.creators[0].share = 100' all/$INDEX.json > tmp.$$.json && mv tmp.$$.json all/$INDEX.json; done
+
+# shuffle
+for i in *.png; do uuid=$(uuidgen); mv "$i" "$uuid.png"; FILENAME=${i/.png/""};  mv "$FILENAME.json" "$uuid.json"; done
+for i in assets_mw/*.png; do uuid=$(uuidgen); mv "$i" assets_mw/"$uuid.png"; FILENAME=${i/.png/""};  mv "$FILENAME.json" assets_mw/"$uuid.json"; done
